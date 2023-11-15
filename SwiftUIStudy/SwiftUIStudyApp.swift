ca9a28d736600ac60617f92205596e8d1072058e
Maksim Kosnikov <maksim.kosnikov@fojin.tech>
2023-11-15 15:05:26 +0300

    Initial Commit

---
+++ SwiftUIStudy/SwiftUIStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyApp.swift
+//  SwiftUIStudy
+//
+//  Created by USER on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
