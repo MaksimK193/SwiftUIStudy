3846beed2a0a2ab23917b89260837ae30c8a9f81
Maksim Kosnikov <maksim.kosnikov@fojin.tech>
2023-12-13 10:18:59 +0300

    rrr

--- SwiftUIStudyUITests/SwiftUIStudyUITests.swift
+++ SwiftUIStudyUITests/SwiftUIStudyUITests.swift
@@ -1,38 +1,38 @@
+////
+////  SwiftUIStudyUITests.swift
+////  SwiftUIStudyUITests
+////
+////  Created by USER on 15.11.2023.
+////
 //
-//  SwiftUIStudyUITests.swift
-//  SwiftUIStudyUITests
+//import XCTest
 //
-//  Created by USER on 15.11.2023.
+//final class SwiftUIStudyUITests: XCTestCase {
+//    var app: XCUIApplication!
+//    
+//    override func setUp() {
+//        app = XCUIApplication()
+//        app.launch()
+//        
+//        app.buttons["menuButton"].tap()
+//    }
 //
-
-import XCTest
-
-final class SwiftUIStudyUITests: XCTestCase {
-    var app: XCUIApplication!
-    
-    override func setUp() {
-        app = XCUIApplication()
-        app.launch()
-        
-        app.buttons["menuButton"].tap()
-    }
-
-    func test_navigation_shouldCorrectOpenView() {
-        let backButton = app.navigationBars.buttons.element(boundBy: 0)
-        
-        app.otherElements.buttons["Core Data"].tap()
-        XCTAssert(app.buttons["coreDataAddButton"].exists)
-        backButton.tap()
-        
-        app.otherElements.buttons["Swift Data"].tap()
-        XCTAssert(app.buttons["swiftDataAddButton"].exists)
-        backButton.tap()
-        
-        app.otherElements.buttons["Weather"].tap()
-        XCTAssert(app.buttons["weatherUpdateButton"].exists)
-        backButton.tap()
-        
-        app.otherElements.buttons["Photo Compression"].tap()
-        XCTAssert(app.buttons["compressPhotoButton"].exists)
-    }
-}
+//    func test_navigation_shouldCorrectOpenView() {
+//        let backButton = app.navigationBars.buttons.element(boundBy: 0)
+//        
+//        app.otherElements.buttons["Core Data"].tap()
+//        XCTAssert(app.buttons["coreDataAddButton"].exists)
+//        backButton.tap()
+//        
+//        app.otherElements.buttons["Swift Data"].tap()
+//        XCTAssert(app.buttons["swiftDataAddButton"].exists)
+//        backButton.tap()
+//        
+//        app.otherElements.buttons["Weather"].tap()
+//        XCTAssert(app.buttons["weatherUpdateButton"].exists)
+//        backButton.tap()
+//        
+//        app.otherElements.buttons["Photo Compression"].tap()
+//        XCTAssert(app.buttons["compressPhotoButton"].exists)
+//    }
+//}
