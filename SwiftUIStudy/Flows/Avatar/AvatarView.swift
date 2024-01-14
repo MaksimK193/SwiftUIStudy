c76d80d70a01fcd6fc995581802817f6963f605a
Maksim Kosnikov <maksim.kosnikov@fojin.tech>
2024-01-14 21:42:50 +0300

    Add saving to database

--- SwiftUIStudy/Flows/Avatar/AvatarView.swift
+++ SwiftUIStudy/Flows/Avatar/AvatarView.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct AvatarView: View {
     @StateObject private var viewModel = AvatarViewModel()
     @State private var inputText: String = ""
-    @State private var image: UIImage = UIImage()
     
     var body: some View {
         VStack {
@@ -18,51 +17,22 @@ struct AvatarView: View {
                 TextField("", text: $inputText)
                     .textFieldStyle(.roundedBorder)
                 Button("", systemImage: "plus") {
-                    drawAvatar(bitMap: convertHashToBitMap(hash: inputText.hashValue))
+                    viewModel.drawAvatar(hash: inputText.hashValue)
                 }
             }
             .padding()
-            Image(uiImage: image)
-            List {
-                ForEach(viewModel.avatars) { avatar in
-                    Text(avatar.id?.uuidString ?? "Unknown")
+            Image(uiImage: viewModel.image)
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(viewModel.avatars) { avatar in
+                        if let data = avatar.avatar {
+                            Image(uiImage: UIImage(data: data) ?? UIImage())
+                        }
+                    }
                 }
             }
         }
     }
-    
-    func convertHashToBitMap(hash: Int) -> [Int]{
-        let flatArray = Array(String(abs(hash)))
-        
-        return flatArray.flatMap {
-            Int(String($0))
-        }
-    }
-    
-    func drawAvatar(bitMap: [Int]) {
-        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 64, height: 64))
-        
-        let colors: [CGColor] = [UIColor.red.cgColor, UIColor.blue.cgColor, UIColor.green.cgColor, UIColor.white.cgColor, UIColor.purple.cgColor, UIColor.gray.cgColor, UIColor.cyan.cgColor, UIColor.magenta.cgColor, UIColor.systemPink.cgColor, UIColor.orange.cgColor]
-
-        let rectangles: [CGRect] = [
-            CGRect(x: 0, y: 0, width: 16, height: 16), CGRect(x: 16, y: 0, width: 16, height: 16), CGRect(x: 32, y: 0, width: 16, height: 16), CGRect(x: 48, y: 0, width: 16, height: 16),
-            CGRect(x: 0, y: 16, width: 16, height: 16), CGRect(x: 16, y: 16, width: 16, height: 16), CGRect(x: 32, y: 16, width: 16, height: 16), CGRect(x: 48, y: 16, width: 16, height: 16),
-            CGRect(x: 0, y: 32, width: 16, height: 16), CGRect(x: 16, y: 32, width: 16, height: 16), CGRect(x: 32, y: 32, width: 16, height: 16), CGRect(x: 48, y: 32, width: 16, height: 16),
-            CGRect(x: 0, y: 48, width: 16, height: 16), CGRect(x: 16, y: 48, width: 16, height: 16), CGRect(x: 32, y: 48, width: 16, height: 16), CGRect(x: 48, y: 48, width: 16, height: 16),
-        ]
-        
-        let img = renderer.image { ctx in
-            for (index, value) in rectangles.enumerated() {
-                let rectangle = value
-                ctx.cgContext.setFillColor(colors[bitMap[index]])
-                ctx.cgContext.addRect(rectangle)
-                ctx.cgContext.drawPath(using: .fill)
-            }
-        }
-
-        image = img
-    }
-    
 }
 
 #Preview {
--- SwiftUIStudy/Flows/Avatar/AvatarViewModel.swift
+++ SwiftUIStudy/Flows/Avatar/AvatarViewModel.swift
@@ -7,10 +7,12 @@
 
 import Foundation
 import CoreData
+import UIKit
 
 class AvatarViewModel: ObservableObject {
     let coreDataManager = CoreDataManager.shared
     @Published var avatars: [AvatarEntity] = []
+    @Published var image: UIImage = UIImage()
     
     init() {
         coreDataManager.setupDataModel(name: .Avatars)
@@ -37,10 +39,10 @@ class AvatarViewModel: ObservableObject {
         }
     }
     
-    func addAvatar(name: String, id: UUID? = UUID()) {
-        let newDepartment = DepartmentEntity(context: coreDataManager.context)
-        newDepartment.id = id
-        newDepartment.name = name
+    func addAvatar(avatar: Data, id: UUID? = UUID()) {
+        let newAvatar = AvatarEntity(context: coreDataManager.context)
+        newAvatar.id = id
+        newAvatar.avatar = avatar
         save()
     }
     
@@ -52,6 +54,38 @@ class AvatarViewModel: ObservableObject {
     func save() {
         coreDataManager.save()
         getEntity()
+    }
+    
+    func drawAvatar(hash: Int) {
+        let flatArray = Array(String(abs(hash)))
+        
+        let bitMap = flatArray.flatMap {
+            Int(String($0))
+        }
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 64, height: 64))
+        
+        let colors: [CGColor] = [UIColor.red.cgColor, UIColor.blue.cgColor, UIColor.green.cgColor, UIColor.white.cgColor, UIColor.purple.cgColor, UIColor.gray.cgColor, UIColor.cyan.cgColor, UIColor.magenta.cgColor, UIColor.systemPink.cgColor, UIColor.orange.cgColor]
+
+        let rectangles: [CGRect] = [
+            CGRect(x: 0, y: 0, width: 16, height: 16), CGRect(x: 16, y: 0, width: 16, height: 16), CGRect(x: 32, y: 0, width: 16, height: 16), CGRect(x: 48, y: 0, width: 16, height: 16),
+            CGRect(x: 0, y: 16, width: 16, height: 16), CGRect(x: 16, y: 16, width: 16, height: 16), CGRect(x: 32, y: 16, width: 16, height: 16), CGRect(x: 48, y: 16, width: 16, height: 16),
+            CGRect(x: 0, y: 32, width: 16, height: 16), CGRect(x: 16, y: 32, width: 16, height: 16), CGRect(x: 32, y: 32, width: 16, height: 16), CGRect(x: 48, y: 32, width: 16, height: 16),
+            CGRect(x: 0, y: 48, width: 16, height: 16), CGRect(x: 16, y: 48, width: 16, height: 16), CGRect(x: 32, y: 48, width: 16, height: 16), CGRect(x: 48, y: 48, width: 16, height: 16),
+        ]
+        
+        let img = renderer.image { ctx in
+            for (index, value) in rectangles.enumerated() {
+                let rectangle = value
+                ctx.cgContext.setFillColor(colors[bitMap[index]])
+                ctx.cgContext.addRect(rectangle)
+                ctx.cgContext.drawPath(using: .fill)
+            }
+        }
+
+        image = img
+        guard let data = img.pngData() else { return }
+        addAvatar(avatar: data)
     }
 }
 
