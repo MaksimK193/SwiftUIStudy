f897ff1cde02db60c4e9ab021ea938ed12500b23
Maksim Kosnikov <maksim.kosnikov@fojin.tech>
2024-01-24 17:10:40 +0300

    Add creating route

--- SwiftUIStudy/Common/Managers/YandexMapLocationManager.swift
+++ SwiftUIStudy/Common/Managers/YandexMapLocationManager.swift
@@ -13,8 +13,11 @@ import YandexMapsMobile
 class YandexMapLocationManager: ObservableObject {
     private let locationManager = LocationManager()
     private let searchManager = YMKSearch.sharedInstance().createSearchManager(with: .combined)
+    let drivingRouter: YMKDrivingRouter = YMKDirections.sharedInstance().createDrivingRouter()
     let mapView: YMKMapView = YMKMapView(frame: CGRect.zero)
     private var searchSession: YMKSearchSession?
+    private var drivingSession: YMKDrivingSession?
+    var routesCollection: YMKMapObjectCollection!
     lazy var map: YMKMap = {
         return mapView.mapWindow.map
     }()
@@ -28,35 +31,15 @@ class YandexMapLocationManager: ObservableObject {
         return options
     }()
     
+    let drivingOptions: YMKDrivingDrivingOptions = {
+        let options = YMKDrivingDrivingOptions()
+        options.routesCount = 3
+        return options
+    }()
+    
     init() {        
         setupLocationUpdates()
-    }
-    
-    func search(withText: String) {
-        searchSession = searchManager.submit(
-            withText: withText,
-            geometry: YMKVisibleRegionUtils.toPolygon(with: map.visibleRegion),
-            searchOptions: searchOptions,
-            responseHandler: handleSearchSessionResponse
-        )
-    }
-    
-    func resubmitAfterMove() {
-        let geometry = YMKVisibleRegionUtils.toPolygon(with: map.visibleRegion)
-        searchSession?.setSearchAreaWithArea(geometry)
-        searchSession?.resubmit(responseHandler: handleSearchSessionResponse)
-    }
-    
-    private func handleSearchSessionResponse(response: YMKSearchResponse?, error: Error?) {
-        if let error {
-            print(error.localizedDescription)
-            return
-        }
-        
-        let geoObjects = response?.collection.children.compactMap { $0.obj }
-        let coordinates = geoObjects?.first?.geometry.first?.point
-        
-        centerMapLocation(target: coordinates, map: mapView)
+        routesCollection = map.mapObjects.add()
     }
     
     func setupLocationUpdates() {
@@ -82,5 +65,76 @@ class YandexMapLocationManager: ObservableObject {
             with: YMKCameraPosition(target: location, zoom: 18, azimuth: 0, tilt: 0),
             animation: YMKAnimation(type: YMKAnimationType.smooth, duration: 0.5)
         )
+    }
+}
+
+extension YandexMapLocationManager {
+    func createRoute(point: YMKPoint) {
+        let points = [
+            YMKRequestPoint(point: YMKPoint(latitude: lastUserLocation.coordinate.latitude, longitude: lastUserLocation.coordinate.longitude), type: .waypoint, pointContext: nil, drivingArrivalPointId: nil),
+            YMKRequestPoint(point: point, type: .waypoint, pointContext: nil, drivingArrivalPointId: nil)
+        ]
+        
+        drivingSession = drivingRouter.requestRoutes(
+            with: points,
+            drivingOptions: drivingOptions,
+            vehicleOptions: YMKDrivingVehicleOptions(),
+            routeHandler: drivingRouteHandler)
+    }
+    
+    func drivingRouteHandler(drivingRoutes: [YMKDrivingRoute]?, error: Error?) {
+        if let error {
+            print(error.localizedDescription)
+            return
+        }
+
+        guard let drivingRoutes else {
+            return
+        }
+        
+        guard let polyline = drivingRoutes.first?.geometry else {
+            return
+        }
+        
+        drawRoute(polyline: polyline)
+    }
+    
+    func drawRoute(polyline: YMKPolyline) {
+        routesCollection.clear()
+        let polylineMapObject = routesCollection.addPolyline(with: polyline)
+        polylineMapObject.strokeWidth = 5.0
+        polylineMapObject.setStrokeColorWith(.gray)
+        polylineMapObject.outlineWidth = 1.0
+        polylineMapObject.outlineColor = .black
+    }
+}
+
+extension YandexMapLocationManager {
+    func search(withText: String) {
+        searchSession = searchManager.submit(
+            withText: withText,
+            geometry: YMKVisibleRegionUtils.toPolygon(with: map.visibleRegion),
+            searchOptions: searchOptions,
+            responseHandler: handleSearchSessionResponse
+        )
+    }
+    
+    func resubmitAfterMove() {
+        let geometry = YMKVisibleRegionUtils.toPolygon(with: map.visibleRegion)
+        searchSession?.setSearchAreaWithArea(geometry)
+        searchSession?.resubmit(responseHandler: handleSearchSessionResponse)
+    }
+    
+    private func handleSearchSessionResponse(response: YMKSearchResponse?, error: Error?) {
+        if let error {
+            print(error.localizedDescription)
+            return
+        }
+        
+        let geoObjects = response?.collection.children.compactMap { $0.obj }
+        let coordinates = geoObjects?.first?.geometry.first?.point
+        
+        centerMapLocation(target: coordinates, map: mapView)
+        createRoute(point: coordinates!)
     }
 }
