2bebbf6eb312c3725cfa926af9ffa0c19df0a5d0
Maksim Kosnikov <maksim.kosnikov@fojin.tech>
2024-02-15 10:45:36 +0300

    Add Realm

--- SwiftUIStudy/Flows/Realm/RealmView.swift
+++ SwiftUIStudy/Flows/Realm/RealmView.swift
@@ -6,17 +6,43 @@
 //
 
 import SwiftUI
+import RealmSwift
 import YandexMobileMetrica
 
 struct RealmView: View {
+    @ObservedResults(Car.self) var cars
+    @State var nameText: String = ""
+    
     var body: some View {
-        Text("Realm")
-            .onAppear {
-                YMMYandexMetrica.reportEvent("RealmScreen opened")
+        HStack {
+            TextField("name", text: $nameText)
+                .textFieldStyle(.roundedBorder)
+            addButton
+        }
+        .padding()
+        List {
+            ForEach(cars) { item in
+                Text(item.name)
             }
+            .onDelete(perform: $cars.remove)
+        }
+        .onAppear {
+            YMMYandexMetrica.reportEvent("RealmScreen opened")
+        }
+    }
+    
+    var addButton: some View {
+        Button {
+            withAnimation {
+                $cars.append(Car(value: ["name": nameText]))
+            }
+        } label: {
+            Image(systemName: "plus")
+        }
     }
 }
 
 #Preview {
     RealmView()
 }
+
